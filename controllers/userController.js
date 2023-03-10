a418a058bb1126968eb2a5fc9333232fcb7d3461
Sayadkhan <abdullahalsayad03@gmail.com>
2023-03-10 09:38:59 +0600

    userController

---
+++ controllers/userController.js
@@ -0,0 +1,52 @@
+const userModel = require("../models/userMode;");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
+const { json } = require("express");
+
+// generate token
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
+
+// register user
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const exist = await userModel.findOne({ email });
+
+    if (exist) {
+      return res.status(400), json("email already exist");
+    }
+
+    if (!name || !email || !password) {
+      return res.status(400).json("all fields are required");
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json("Invalid email.");
+    }
+
+    if (!validator.isStrongPassword(password)) {
+      return res.status(400).json("password must be strong");
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    // creat an user
+    const user = await userModel.create({ name, email, password: hash });
+
+    // creat a tokan
+    const tokan = createToken(user._id);
+
+    res.status(200), json({ _id: user._id, name, email, password, tokan });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
+
+module.exports = { registerUser };
